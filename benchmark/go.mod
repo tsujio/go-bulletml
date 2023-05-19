1ec751c8379d67807a5960c4a7331faae6dd8fe1
tsujio <ntsujio@gmail.com>
2023-05-19 14:36:15 +0900

    Add benchmark

---
+++ benchmark/go.mod
@@ -0,0 +1,7 @@
+module github.com/tsujio/go-bulletml/benchmark
+
+go 1.19
+
+require github.com/tsujio/go-bulletml v0.0.0-00010101000000-000000000000
+
+replace github.com/tsujio/go-bulletml => ../
---
+++ benchmark/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/tsujio/go-bulletml"
+)
+
+var testCases = map[string]string{
+	"nop": `
+	<bulletml>
+		<action label="top">
+			<repeat>
+				<times>1000</times>
+				<action>
+					<fire>
+						<bullet />
+					</fire>
+				</action>
+			</repeat>
+		</action>
+	</bulletml>
+	`,
+
+	"repeat": `
+	<bulletml>
+		<action label="top">
+			<repeat>
+				<times>1000</times>
+				<action>
+					<fire>
+						<bullet>
+							<action>
+								<repeat>
+									<times>1000</times>
+									<action>
+									</action>
+								</repeat>
+							</action>
+						</bullet>
+					</fire>
+				</action>
+			</repeat>
+		</action>
+	</bulletml>
+	`,
+}
+
+func main() {
+	source, exists := testCases[os.Args[1]]
+	if !exists {
+		var tc []string
+		for k, _ := range testCases {
+			tc = append(tc, k)
+		}
+		panic("Please choose from: " + strings.Join(tc, ", "))
+	}
+
+	bml, err := bulletml.Load(bytes.NewReader([]byte(source)))
+	if err != nil {
+		panic(err)
+	}
+
+	var runners []bulletml.BulletRunner
+
+	runner, err := bulletml.NewRunner(bml, &bulletml.NewRunnerOptions{
+		OnBulletFired: func(bulletRunner bulletml.BulletRunner, _ *bulletml.FireContext) {
+			runners = append(runners, bulletRunner)
+		},
+		CurrentShootPosition:  func() (float64, float64) { return 0, 0 },
+		CurrentTargetPosition: func() (float64, float64) { return 0, 0 },
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	if err := runner.Update(); err != nil {
+		panic(err)
+	}
+
+	const loop = 10000
+
+	start := time.Now().UnixNano()
+
+	for i := 0; i < loop; i++ {
+		for _, r := range runners {
+			if err := r.Update(); err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	end := time.Now().UnixNano()
+
+	json.NewEncoder(os.Stdout).Encode(map[string]any{
+		"testCase":    os.Args[1],
+		"bulletCount": len(runners),
+		"loopCount":   loop,
+		"elapsedNano": end - start,
+	})
+}
